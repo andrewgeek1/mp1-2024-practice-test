eb3e6700c2a5c3f7aea3efc98e11773020bdf403
andrewgeek1 <andrewfugi64@gmail.com>
2024-12-19 21:15:56 +0300

    Update task3.c

--- 3824Б1ПР5/24 Фугаров Андрей Сергеевич/task3.c
+++ 3824Б1ПР5/24 Фугаров Андрей Сергеевич/task3.c
@@ -1,6 +1,16 @@
 // Задача 3. Дана строка s.
 // Подсчитать количество содержащихся в ней строчных букв английского алфавита.
+#include <ctype.h>
 size_t task3(char *s)
 {
-  return -1;
+  size_t count = 0;
+    
+    while (*s != '\0') {
+        if (islower((unsigned char)*s)) {
+            count++;
+        }
+        s++;
+    }
+    
+    return count;
 }
